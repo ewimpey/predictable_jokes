5db68f26a96b270b413668d449d47201a84e6e8b
Rui Costa <costa.ruitiago@gmail.com>
2025-07-22 11:49:49 +0200

    creates main.py

---
+++ docker/main.py
@@ -0,0 +1,16 @@
+from predictable_jokes.jokes import tell_joke
+import os
+
+def get_topic():
+    if os.getenv("TOPIC"):
+        os.getenv("TOPIC")
+    else:
+        return "data science"
+    
+def get_complexity():
+    if os.getenv("COMPLEXITY"):
+        return os.getenv("COMPLEXITY")
+    else:
+        return "medium"
+
+tell_joke(topic=get_topic(), complexity=get_complexity())
